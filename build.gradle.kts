7b539027b19adb20dd73d65a673ae23c7628bdcf
swnck <datapack3t.contact@gmail.com>
2024-03-22 00:38:59 +0100

    First push

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "de.swnck.JxRequest"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.reflections:reflections:0.9.12")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "nexus"
+            val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
+            url = if (isSnapshot)
+                uri("https://nexus.synclyn.com/repository/maven-snapshots/")
+            else
+                uri("https://nexus.synclyn.com/repository/maven-releases/")
+            credentials {
+                username = System.getenv("NEXUS_USERNAME")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
---
+++ src/main/java/de/swnck/JxRequest.java
@@ -0,0 +1,7 @@
+package de.swnck;
+
+public class JxRequest {
+    public static void main(String[] args) {
+
+    }
+}
